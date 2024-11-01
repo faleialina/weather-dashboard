f1a62f6975e5ea460e3b6037f8235dca3aa28a2b
faleialina <august3@mail.ru>
2024-11-01 19:08:09 +0300

    adding fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,32 @@ const apiKey = '59247bfe3dff1b7dacdacf06de546a1b'
 function App() {
   const [cityName, setCityName] = useState(localStorage.getItem('nameCity') || '')
   const [weathe, setWeather] = useState({})
+  const [flagCheckNameCity, setFlagCheckNameCity] = useState(true)
 
 
   const response = async () => {
-    const data = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
-    setWeather(data.data)
+    try {
+      const data = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+      setFlagCheckNameCity(true)
+      localStorage.setItem('nameCity', cityName)
+      setWeather(data.data)
+    } catch (error) {
+      console.log(error.message);
+      setFlagCheckNameCity(false)
+    }
   }
+
 
   useEffect(() => {
     response()
   }, [cityName])
 
+
   return (
     <>
       <video src={background} autoPlay muted loop className='background' />
       <div className='wrapper'>
-        <Search setCityName={setCityName} setWeather={setWeather} />
+        <Search setCityName={setCityName} flagCheckNameCity={flagCheckNameCity} setWeather={setWeather} setFlagCheckNameCity={setFlagCheckNameCity} />
         <Form weathe={weathe} />
       </div>
     </>
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 import styles from './style.module.css'
 
-function SearchForm({ setCityName, setWeather }) {
+function SearchForm({ setCityName, flagCheckNameCity, setWeather, setFlagCheckNameCity }) {
     const [city, setCity] = useState('')
 
     return (
         <div className={styles.wrapper}>
             <h1 className={styles.text}>Weather</h1>
-            <button className={styles.weatherButton} onClick={() => {
-                setCity('')
-                setWeather({})
-            }}>Reset</button>
-            <input className={styles.weatherInput} type="text" onChange={(e) => setCity(e.target.value)} placeholder="Your city" value={city} />
-            <button className={styles.weatherButton} onClick={() => {
-                setCityName(city)
-                localStorage.setItem('nameCity', city)
-            }}>Search</button>
+            <div className={styles.wrapperInpAndErr}>
+                <input className={styles.weatherInput} type="text" onChange={(e) => setCity(e.target.value)} placeholder="Your city" value={city} />
+                {!flagCheckNameCity ? <p className={styles.errorName}>The name of the city is introduced incorrectly</p> : null}
+            </div>
+            <div>
+                <button className={styles.weatherButton} onClick={() => {
+                    setCity('')
+                    setFlagCheckNameCity(true)
+                }}>Reset</button>
+                <button className={styles.weatherButton} onClick={() => {
+                    setCityName(city)
+                }}>Search</button>
+            </div>
         </div>
     );
 }
